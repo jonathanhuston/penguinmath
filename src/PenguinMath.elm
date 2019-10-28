4a3b52b8d4d136c5ab121df1ba4dfe653401fb31
Jonathan Huston <jonathan.huston@gmail.com>
2019-10-28 09:17:04 -0700

    adjusted penguin dance

--- src/PenguinMath.elm
+++ src/PenguinMath.elm
@@ -10,24 +10,69 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
 
+-- MAIN
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+    
+
+-- MODEL
+
+type Model
+    = Stand
+    | Dance
+
+init: Model
+init = 
+    Stand
+
+
+-- UPDATE
+
+type Msg 
+    = Switch
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Switch ->
+            case model of
+                Stand ->
+                    Dance
+
+                Dance ->
+                    Stand
+
+
+-- VIEW
+
+view : Model -> Html Msg
 view model =
-    div [ class "content" ]
+    div [ id "content" ]
         [ h1 [] [ text "Penguin Math" ]
-        , a [ href "https://lunamintpop.neocities.org" ]
-            [ img 
-                [ src "resources/pengi.png"
-                , height 130
-                ] []
+        , p [] [ text "Hi, I'm Pengi the Penguin. Let's do some math."]
+        , section [ id "pengi" , class "container"]
+            [ button 
+                [ onClick Switch ]
+                [ viewPengi model ]
             ]
-        , video 
-            [ src "resources/pengi.mov"
-            , height 150
-            , autoplay True
-            , loop True
-            , controls True
-            ] []
         ]
 
 
-main =
-    view "no model yet"
+viewPengi : Model -> Html Msg
+viewPengi model =
+    case model of 
+        Stand ->
+            img 
+                [ src "resources/pengi.png"
+                , height 130
+                ] []
+
+        Dance ->
+            video 
+                [ src "resources/pengi.mov"
+                , height 150
+                , autoplay True
+                , loop True
+                , controls False
+                ] []

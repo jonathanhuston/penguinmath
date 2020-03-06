709c8e75ab85437a0f9fa1dda2071c3a899b20b1
Jonathan Huston <jonathan.huston@gmail.com>
2020-03-06 12:52:35 -0800

    added division1000

---
+++ penguinmath/lib/division.py
@@ -0,0 +1,22 @@
+# division tables
+
+import operator
+import random
+
+ops = {'/': operator.truediv}
+
+
+def get_qa(num_range):
+    op_sym, op = random.choice(list(ops.items()))
+    while True:
+        x = random.randint(0, num_range + 1)
+        y = random.randint(0, num_range + 1)
+        if (op_sym == "/") and (y == 0):
+            continue
+        a = op(x, y)
+        if (a <= num_range) and (a == round(a)):
+            break
+    
+    q = "{} {} {} = ?".format(x, op_sym, y)
+
+    return q, str(round(a))
--- penguinmath/lib/generate.py
+++ penguinmath/lib/generate.py
@@ -1,6 +1,14 @@
-from lib import volumes, weights, addition, measures, simple, time_dhms, times
+from lib import volumes, weights, addition, measures, simple, time_dhms, times, division
 
 quizzes = [
+    {
+        "name": "times1000",
+        "title": "Times tables (up to 1000)"
+    },
+    {
+        "name": "division1000",
+        "title": "Division (up to 1000)"
+    },
     {
         "name": "volumes",
         "title": "Volumes (hl, l, dl, cl, ml)"
@@ -20,10 +28,6 @@ quizzes = [
     {
         "name": "times1000000",
         "title": "Times tables (up to 1000000)"
-    },
-    {
-        "name": "times1000",
-        "title": "Times tables (up to 1000)"
     },
     {
         "name": "times100",
@@ -69,6 +73,7 @@ def get_quiz(quiz=quizzes[0]):
         "times1000000": {"generate_qas": generate_qas, "get_qa": times.get_qa, "total": 10, "goal": 9, "extra": 1000000},
         "times1000": {"generate_qas": generate_qas, "get_qa": times.get_qa, "total": 25, "goal": 23, "extra": 1000},
         "times100": {"generate_qas": generate_qas, "get_qa": times.get_qa, "total": 25, "goal": 23, "extra": 100},
+        "division1000": {"generate_qas": generate_qas, "get_qa": division.get_qa, "total": 25, "goal": 23, "extra": 1000},
         "capitals": {"generate_qas": simple.generate_qas, "get_qa": None, "total": 40, "goal": 38, "extra": "capitals"},
         "roman": {"generate_qas": simple.generate_qas, "get_qa": None, "total": 14, "goal": 13, "extra": "roman"},
         "addition100000": {"generate_qas": generate_qas, "get_qa": addition.get_qa, "total": 10, "goal": 9, "extra": 100000}
